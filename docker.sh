d684b7298dae26ebe45cd9beb8d4d800fe91829f
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2022-08-10 10:24:07 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/
 
-docker build -t nodeimage_`date +\%d_\%m` .
+docker build -t nodeimage_`date +%Y%m%d%H%M%S%N` .
 
 docker run -d --name dockercontainer_`date +\%d_\%m` -p 5000:5000 nodeimage
 

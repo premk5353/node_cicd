7b1a53c675ed54b928b52fd6851cbdcdfa08cc7a
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2022-08-10 10:04:20 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@ cd /var/www/html/
 
 docker build -t nodeimage .
 
-docker run -d --name nodecontainer -p 5000:5000 nodeimage
+docker run -d --name dockercontainer -p 5000:5000 nodeimage
 
 docker ps

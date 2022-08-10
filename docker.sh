d8bbcd4ffe0fd0189d3aa0165770a6536ec8f3b2
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2022-08-10 10:56:26 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@ cd /var/www/html/
 
 docker build -t nodeimage_`date +%Y%m%d%H%M%S%N` .
 
-fuser -k 5000/tcp || true
+sudo kill `fuser -k 5000/tcp || true`
 
 docker run -d --name dockercontainer_`date +%Y%m%d%H%M%S%N` -p 5000:5000 nodeimage
 
